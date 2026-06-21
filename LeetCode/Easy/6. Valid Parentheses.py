3ac3f69a7ef6cb8d5f0cbcf7cca0f55775a36708
SubrahmanyamPeddiraju <159891956+SubrahmanyamPeddiraju@users.noreply.github.com>
2026-06-21 22:25:20 +0530

    Create 6. Valid Parentheses.py

---
+++ LeetCode/Easy/6. Valid Parentheses.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        for i in s:
+            if i == '(' or i == '[' or i == '{':
+                stack.append(i)
+            else:
+                if len(stack) == 0:
+                    return False
+                elif (stack[-1]=='(' and i==')') or (stack[-1]=='[' and i==']') or (stack[-1]=='{' and i=='}'):
+                    stack.pop()
+                else:
+                    return False
+        return len(stack) == 0
