58f210799a56fa883fb3cb299f7cc65020a83b9b
SubrahmanyamPeddiraju <159891956+SubrahmanyamPeddiraju@users.noreply.github.com>
2026-06-24 15:07:52 +0530

    Create Binary tree level order traversal.py

---
+++ LeetCode/Medium/Binary tree level order traversal.py
@@ -0,0 +1,19 @@
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        result = []
+        def dfs(node, level):
+            if node is None:
+                return
+            if level == len(result):
+                result.append([])
+            result[level].append(node.val)
+            dfs(node.left, level+1)
+            dfs(node.right, level+1)
+        dfs(root, 0)
+        return result
