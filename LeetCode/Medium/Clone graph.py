04875e1940a47f6ae6d658672510a93d8a9d26b5
SubrahmanyamPeddiraju <159891956+SubrahmanyamPeddiraju@users.noreply.github.com>
2026-07-23 17:58:44 +0530

    Create Clone graph.py

---
+++ LeetCode/Medium/Clone graph.py
@@ -0,0 +1,21 @@
+# Definition for a Node.
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+"""
+
+class Solution:
+    def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
+        if node is None:
+            return None
+        visited = {}
+        def dfs(current):
+            if current in visited:
+                return visited[current]
+            copy = Node(current.val)
+            visited[current] = copy
+            for neighbor in current.neighbors:
+                copy.neighbors.append(dfs(neighbor))
+            return copy
+        return dfs(node)
